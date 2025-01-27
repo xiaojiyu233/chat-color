53ab63c1ff310c4716dc4d59957cda3ca9ed6381
xiaojiyu233 <53163277+xiaojiyu233@users.noreply.github.com>
2025-01-27 20:33:09 +0800

    Delete app/styles/build.sh

--- app/styles/build.sh
+++
@@ -1,2 +0,0 @@
-#docker build -t bili-rick . && docker run --name bili-rick -d -p 8022:80 bili-rick
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/rickcert/chatgpt-next-web . --push
