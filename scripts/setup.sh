f9e46ab69d61a81b280c3cfb954ed0bd274afe8f
xiaojiyu233 <53163277+xiaojiyu233@users.noreply.github.com>
2025-01-27 20:50:12 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -54,8 +54,9 @@ if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v y
 fi
 
 # Clone the repository and install dependencies
-git clone https://github.com/ChatGPTNextWeb/ChatGPT-Next-Web
-cd ChatGPT-Next-Web
+#git clone https://github.com/ChatGPTNextWeb/ChatGPT-Next-Web
+git clone https://github.com/xiaojiyu233/chat-color
+cd chat-color
 yarn install
 
 # Prompt user for environment variables
